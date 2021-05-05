dba72e71f61fd47686a8a6cc45a7340d3034140a
Oliver Lippert <oliver@lipperts-web.de>
2021-05-05 23:09:12 +0200

    Add script to parse WordPress plugin tags for compatibility information
    Per tag, list date, min WordPress version, max tested WordPress version and required PHP version.
    
    Signed-off-by: Oliver Lippert <oliver@lipperts-web.de>

---
+++ list-plugin-update-info.sh
@@ -0,0 +1,34 @@
+prefix="plugins/"
+
+function checkoutPlugin() {
+    for tag in $(svn list "https://plugins.svn.wordpress.org/$1/tags") ; do
+      [ ! -d "$prefix$1/$tag" ] || continue
+
+      echo "$1/$tag"
+      mkdir -p "$prefix$1/$tag"
+      svn checkout "https://plugins.svn.wordpress.org/$1/tags/$tag" "$prefix$1/$tag" --depth empty -q
+      svn up "$prefix$1/$tag/readme.txt" -q
+      svn up "$prefix$1/$tag/README.txt" -q
+
+      # If readme is uppercase, lowercase the filename (WSL-safe)
+      [ -f "$prefix$1/$tag/README.txt" ] || continue
+      mv "$prefix$1/$tag/README.txt" "$prefix$1/$tag/readme2.txt"
+      mv "$prefix$1/$tag/readme2.txt" "$prefix$1/$tag/readme.txt"
+    done
+}
+
+function listVersions() {
+  for f in $(find "$prefix$1" -maxdepth 1 -type d | grep "/$2" | sort --version-sort) ; do
+    [ -f "$f/readme.txt" ] || continue
+
+    echo "$f"
+    svn info "$f" | grep "Changed Date"
+    grep "Requires at least:" "$f/readme.txt"
+    grep "Tested up to:" "$f/readme.txt"
+    grep "Requires PHP:" "$f/readme.txt"
+    echo " "
+  done
+}
+
+checkoutPlugin "$1"
+listVersions "$1" "$2"
