606af122a10b6af4027735a2e01315521ba80d29
Oliver Lippert <oliver@lipperts-web.de>
2021-05-05 23:09:21 +0200

    Add script to parse WordPress plugin tags for compatibility information
    Per tag, list date, min WordPress version, max tested WordPress version and required PHP version.
    
    Signed-off-by: Oliver Lippert <oliver@lipperts-web.de>

--- list-plugin-update-info.sh
+++ list-plugin-update-info.sh
@@ -1,24 +1,24 @@
 prefix="plugins/"
 
 function checkoutPlugin() {
-    for tag in $(svn list "https://plugins.svn.wordpress.org/$1/tags") ; do
-      [ ! -d "$prefix$1/$tag" ] || continue
+  for tag in $(svn list "https://plugins.svn.wordpress.org/$1/tags"); do
+    [ ! -d "$prefix$1/$tag" ] || continue
 
-      echo "$1/$tag"
-      mkdir -p "$prefix$1/$tag"
-      svn checkout "https://plugins.svn.wordpress.org/$1/tags/$tag" "$prefix$1/$tag" --depth empty -q
-      svn up "$prefix$1/$tag/readme.txt" -q
-      svn up "$prefix$1/$tag/README.txt" -q
+    echo "$1/$tag"
+    mkdir -p "$prefix$1/$tag"
+    svn checkout "https://plugins.svn.wordpress.org/$1/tags/$tag" "$prefix$1/$tag" --depth empty -q
+    svn up "$prefix$1/$tag/readme.txt" -q
+    svn up "$prefix$1/$tag/README.txt" -q
 
-      # If readme is uppercase, lowercase the filename (WSL-safe)
-      [ -f "$prefix$1/$tag/README.txt" ] || continue
-      mv "$prefix$1/$tag/README.txt" "$prefix$1/$tag/readme2.txt"
-      mv "$prefix$1/$tag/readme2.txt" "$prefix$1/$tag/readme.txt"
-    done
+    # If readme is uppercase, lowercase the filename (WSL-safe)
+    [ -f "$prefix$1/$tag/README.txt" ] || continue
+    mv "$prefix$1/$tag/README.txt" "$prefix$1/$tag/readme2.txt"
+    mv "$prefix$1/$tag/readme2.txt" "$prefix$1/$tag/readme.txt"
+  done
 }
 
 function listVersions() {
-  for f in $(find "$prefix$1" -maxdepth 1 -type d | grep "/$2" | sort --version-sort) ; do
+  for f in $(find "$prefix$1" -maxdepth 1 -type d | grep "/$2" | sort --version-sort); do
     [ -f "$f/readme.txt" ] || continue
 
     echo "$f"
